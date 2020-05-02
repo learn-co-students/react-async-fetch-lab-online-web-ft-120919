3ef1a82ca0489f733537c72d78c1bcd9135d6981
Olusola Olagoroye <solaolagoroye@gmail.com>
2020-05-01 22:13:37 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, { Component } from 'react'
+
+export class App extends Component {
+
+  
+
+    render() {
+        return (
+            <div>
+                <h3>Hi!</h3>
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+          .then(response => response.json())
+          .then(data => {
+            this.setState({
+              peopleInSpace: data.people
+            })
+          })
+      }
+
+
+}
+
+export default App
